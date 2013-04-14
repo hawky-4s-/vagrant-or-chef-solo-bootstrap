97c4dc03679988be4499b1c48776bfb123bcecac
Christian Lipphardt <christian.lipphardt@camunda.com>
2013-04-14 23:26:51 +0200

    WIP

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,20 @@ host="${1:-aaron@192.168.1.39}"
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
 ssh-keygen -R "${host#*@}" 2> /dev/null
- 
+
+# Upload our ssh key so we can stop typing the remote password:
+if [ -e "$HOME/.ssh/id_rsa.pub" ]; then
+  cat "$HOME/.ssh/id_rsa.pub" | ssh -o 'StrictHostKeyChecking no' "$host" 'mkdir -m 700 -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
+fi
+
+#tar cjh . | ssh -o 'StrictHostKeyChecking no' "$host" '
+#rm -rf /tmp/chef &&
+#mkdir /tmp/chef &&
+#cd /tmp/chef &&
+#tar xj &&
+#bash install.sh'
+
+# Use this version if you don't ssh in as root (e.g. on EC-2):
 tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
 sudo rm -rf ~/chef &&
 mkdir ~/chef &&
--- install.sh
+++ install.sh
@@ -2,18 +2,38 @@
  
 # This runs as root on the server
  
-chef_binary=/usr/local/bin/chef-solo
+#chef_binary=/usr/local/bin/chef-solo
+chef_binary=~/.rbenv/shims/chef-solo
  
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
     export DEBIAN_FRONTEND=noninteractive
     # Upgrade headlessly (this is only safe-ish on vanilla systems)
     aptitude update &&
-    apt-get -o Dpkg::Options::="--force-confnew" \
-        --force-yes -fuy dist-upgrade &&
+    aptitude safe-upgrade -fy
+    #apt-get -o Dpkg::Options::="--force-confnew" \
+    #    --force-yes -fuy dist-upgrade &&
+    
     # Install Ruby and Chef
-    aptitude install -y ruby1.9.1 ruby1.9.1-dev make &&
-    sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
+
+    aptitude install zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev libruby1.9.1 libreadline-dev git-core make make-doc -y
+
+    cd ~
+    git clone git://github.com/sstephenson/rbenv.git .rbenv
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    exec $SHELL # Restart the shell
+    mkdir -p ~/.rbenv/plugins
+    cd ~/.rbenv/plugins
+    git clone git://github.com/sstephenson/ruby-build.git
+    git clone git://github.com/sstephenson/rbenv-gem-rehash.git
+    rbenv install 1.9.3-p362
+    rbenv rehash
+    rbenv global 1.9.3-p362
+
+    gem update --no-rdoc --no-ri
+    gem install ohai chef --no-rdoc --no-ri
+
 fi &&
  
 "$chef_binary" -c solo.rb -j solo.json
