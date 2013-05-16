a533ac11ec652a52c366bb0e3bcade4463c8e1f5
Christian Lipphardt <christian.lipphardt@camunda.com>
2013-05-16 07:15:08 +0200

    Added site-cookbooks dir

---
+++ chef/site-cookbooks/jbossas7/metadata.rb
@@ -0,0 +1,8 @@
+name             "jbossas7"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures jbossas7"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
