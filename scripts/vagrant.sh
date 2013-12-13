6dec159325db02d7d35d66460ac421f9f3e7fd0a
Christian Lipphardt <christian.lipphardt@googlemail.com>
2013-12-13 11:11:09 +0100

    chore(vagrant): changed to bintray dl mechanism

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -5,36 +5,37 @@ set -x
 
 if [[ $_ != $0 ]]; then
   echo "Script is being sourced"
-  SCRIPT_DIR=$(dirname "${BASH_SOURCE}")
+  SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
 else
   echo "Script is being run"
-  SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+  SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 fi
 
 if [ ! -n "${ENV_BASH_SETTINGS}" ]; then
-  source ${SCRIPT_DIR}/environment.sh
+  source "${SCRIPT_DIR}/environment.sh"
 fi
 
 # vagrant version and package download url
 VAGRANT_GITHUB="mitchellh/vagrant"
-VAGRANT_VERSION=$(curl -s https://api.github.com/repos/${VAGRANT_GITHUB}/git/refs/tags \
+VAGRANT_VERSION="$(curl -s https://api.github.com/repos/${VAGRANT_GITHUB}/git/refs/tags \
 | ruby -rjson -e '
   j = JSON.parse(STDIN.read);
   version = j.map { |t| t["ref"].split("/").last }.sort.last
   puts version[1, version.length]
-')
-VAGRANT_RELEASE_UUID=$(curl -s https://api.github.com/repos/${VAGRANT_GITHUB}/git/refs/tags \
+')"
+VAGRANT_RELEASE_UUID="$(curl -s https://api.github.com/repos/${VAGRANT_GITHUB}/git/refs/tags \
 | ruby -rjson -e '
   j = JSON.parse(STDIN.read);
   version = j.map { |t| t["ref"].split("/").last }.sort.last
   commit = j.find { |t| t["ref"].include? version }
   puts commit["object"]["sha"]
-')
-VAGRANT_RELEASE_URL="http://files.vagrantup.com/packages/${VAGRANT_RELEASE_UUID}"
+')"
+VAGRANT_RELEASE_URL="https://dl.bintray.com/${VAGRANT_GITHUB}"
 
 VAGRANT_FILE="vagrant_${VAGRANT_VERSION}_${ENV_ARCH}.${ENV_PACKAGE_FORMAT}"
 mkdir -p "${BOOTSTRAP_TMP_DIR}"
 wget ${VAGRANT_RELEASE_URL}/${VAGRANT_FILE} -O ${BOOTSTRAP_TMP_DIR}/${VAGRANT_FILE}
+
 
 if [ "${ENV_PACKAGE_FORMAT}" = "deb" ]; then
     sudo dpkg -i ${BOOTSTRAP_TMP_DIR}/${VAGRANT_FILE}
